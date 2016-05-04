ab83834b368b6d59830e2cfb0655917de60561cc
James Bisset <james@bisset.com>
2016-05-05 00:36:35 +0100

    numerous updates to the SASS structure, mobile first responsive and dragging the H1 into the node template where appropriate

--- mcstarter/templates/page.tpl.php
+++ mcstarter/templates/page.tpl.php
@@ -133,11 +133,13 @@
 <div class="tabs clearfix"><?php print render($tabs); ?></div>
 <?php endif; ?>
 
+<?php if(!isset($node)): ?>
 <?php print render($title_prefix); ?>
-<?php if ($title): ?>
-<h1 class="title" id="page-title"><?php print $title; ?></h1>
-<?php endif; ?>
+  <?php if ($title): ?>
+  <h1 class="title" id="page-title"><?php print $title; ?></h1>
+  <?php endif; ?>
 <?php print render($title_suffix); ?>
+<?php endif; ?>
 
 <?php print render($page['help']); ?>
 
